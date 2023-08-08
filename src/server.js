fabf0fba0facae93494fe743945339cfe89e76a4
Arthur Machado <arthur.machado@iclubs.tech>
2023-08-08 00:35:32 -0300

    creating and getting user

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import http from 'node:http'
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
 
-  if(method === 'GET' && url === '/teste') {
-    return response.end('Testando o metodo GET');
+  if(method === 'GET' && url === '/users') {
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
-  if(method === 'POST' && url === '/teste') {
-    return response.end('Testando o metodo POST');
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com"
+    })
+    
+    return response.end('Criação de usuário');
   }
 
   return response.end('Hello World');
