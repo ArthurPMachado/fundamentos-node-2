9b86914837da29454488f6e6bb3b8e5e1dab3ce8
Arthur Machado <arthur.machado@iclubs.tech>
2023-08-07 23:52:44 -0300

    testing POST and GET methods

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from 'node:http'
 
 const server = http.createServer((request, response) => {
-  return response.end('Hello World')
+  const { method, url } = request;
+
+  if(method === 'GET' && url === '/teste') {
+    return response.end('Testando o metodo GET');
+  }
+
+  if(method === 'POST' && url === '/teste') {
+    return response.end('Testando o metodo POST');
+  }
+
+  return response.end('Hello World');
 })
 
 server.listen(3333);
